bd6c4469bedaaa5254cc6c40795ae4840e3116fa
Stewart Hering <heringste@sehs.net>
2015-12-09 13:39:36 -0500

    new level logic
    its awesome

--- game.rb
+++ game.rb
@@ -6,16 +6,35 @@ class Window < Gosu::Window
   def initialize()
     super(1920,1080,true)
     self.caption = "The Rage of Morbork"
-    @scene = MainMenu.new()
+    @level = Level.new()
     @player = Player.new(self)
+  end
+
+  def changeScene()
+    if @level.levelNumber == 0
+      @level.mainMenu()
+    elsif @level.levelNumber == 1
+      @level.intro()
+    elsif @level.levelNumber == 2
+      @level.level1()
+    elsif @level.levelNumber == 3
+      @level.level2()
+    elsif @level.levelNumber == 4
+      @level.level3()
+    elsif @level.levelNumber == 5
+      @level.level4()
+    elsif @level.levelNumber == 6
+      @level.end()
+    end
   end
 
   def draw()
     @player.draw
+    @level.draw
   end
 
   def update()
-    
+    changeScene() if @player.x >= 1900 && #0 enemys left
   end
 end
 
--- levels.rb
+++ levels.rb
@@ -1,72 +1,45 @@
 #Parent Class
 class Level
   def initialize()
-    @player = player
-    @scene = scene
     @background = #something.png
-    @advance = #something that when they complete it they advance
-    @enemies = enemy
-    @menu = menu
     @foreground = #something.png
-    @a_keen_sense_of_optimism = false
-    @ground = ground #just the base x value so that we can use it
+    @a_keen_sense_of_optimism = true
+    @ground = 0 #just the base x value so that we can use it
+    @enemyNumber = 0
+    @levelNumber = 0
   end
 
-  def nextScene
+  def mainMenu() #0
+
+  end
+
+  def intro() #1
+
+  end
+
+  def level1() #2
+
+  end
+
+  def level2() #3
+
+  end
+
+  def level3() #4
+
+  end
+
+  def level4() #5
+
+  end
+
+  def end() #6
+
+  end
+
+  attr_reader
+
+  def draw()
+
   end
 end
-  #Main Menu Child Class
-  class MainMenu < Level
-    @start = start
-    @save = save
-    @exit = break(0)
-    @options = options
-    @newgame = newgame
-    @continuegame = continuegame
-  end
-
-  #Intro Cut Scene Child Class
-  class Intro < Level
-    @music = music
-  end
-
-  #Level 1 Child Class
-  class Level1 < Level
-    @enemies = enemies
-    @advance = #whatever they should advance with
-    @background = #specific background
-  end
-
-  #Level 2 Child Class
-  class Level2 < Level
-    @enemies = enemies
-    @advance = #whatever they should advance with
-    @background = background
-  end
-
-  #Level 3 Child Class
-  class Level3 < Level
-    @enemies = enemies
-    @advance = #whatever they advnace to the next le vel with
-  end
-
-  #Level 4 Child Class
-  class Level4 < Level
-    BOSS
-    @boss = boss
-    @music = scary music
-    @mood = scared
-    @background = hell
-    @enemies = whatever the boss drops
-    @advance = endd of the game
-  end
-
-  # Fin Cut Scene Child Class
-  class Fin < Level
-    @music = happy
-    @mood = good
-    @a_keen_sense_of_optimism = true
-    @an_even_keener_sense_of_optimism = true 
-    @background = happy background
-    @endingmessage = whatever the ending message is
-  end
